617fe15bd91aef805afb9cd5223f256a2d80fe44
wangyi <wwyy456258@126.com>
2022-05-31 09:31:15 +0800

    改造AviaterRegexFilter的pattern匹配方式

--- filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterRegexFilter.java
+++ filter/src/main/java/com/alibaba/otter/canal/filter/aviater/AviaterRegexFilter.java
@@ -14,33 +14,37 @@ import com.alibaba.otter.canal.filter.CanalEventFilter;
 import com.alibaba.otter.canal.filter.exception.CanalFilterException;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.Expression;
+import org.springframework.util.CollectionUtils;
 
 /**
  * 基于aviater进行tableName正则匹配的过滤算法
- * 
+ *
  * @author jianghang 2012-7-20 下午06:01:34
  */
 public class AviaterRegexFilter implements CanalEventFilter<String> {
 
-    private static final String             SPLIT             = ",";
-    private static final String             PATTERN_SPLIT     = "|";
-    private static final String             FILTER_EXPRESSION = "regex(pattern,target)";
-    private static final RegexFunction      regexFunction     = new RegexFunction();
-    private final Expression                exp               = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
+    private static final String SPLIT = ",";
+    private static final String PATTERN_SPLIT = "|";
+    private static final String FILTER_EXPRESSION = "regex(pattern,target)";
+    private static final RegexFunction regexFunction = new RegexFunction();
+    private final Expression exp = AviatorEvaluator.compile(FILTER_EXPRESSION, true);
+
     static {
         AviatorEvaluator.addFunction(regexFunction);
     }
 
-    private static final Comparator<String> COMPARATOR        = new StringComparator();
+    private static final Comparator<String> COMPARATOR = new StringComparator();
 
-    final private String                    pattern;
-    final private boolean                   defaultEmptyValue;
+    final private List<String> patterns;
+    final private boolean defaultEmptyValue;
 
-    public AviaterRegexFilter(String pattern){
+    private static final int patternMaxLength = 40000;
+
+    public AviaterRegexFilter(String pattern) {
         this(pattern, true);
     }
 
-    public AviaterRegexFilter(String pattern, boolean defaultEmptyValue){
+    public AviaterRegexFilter(String pattern, boolean defaultEmptyValue) {
         this.defaultEmptyValue = defaultEmptyValue;
         List<String> list = null;
         if (StringUtils.isEmpty(pattern)) {
@@ -56,33 +60,58 @@ public class AviaterRegexFilter implements CanalEventFilter<String> {
         Collections.sort(list, COMPARATOR);
         // 对pattern进行头尾完全匹配
         list = completionPattern(list);
-        this.pattern = StringUtils.join(list, PATTERN_SPLIT);
+        List<String> patterns = new ArrayList<String>();
+        splitPattern(list, patterns);
+        this.patterns = patterns;
+    }
+
+    private static void splitPattern(List<String> head, List<String> patterns) {
+        if(CollectionUtils.isEmpty(head)){
+            return;
+        }
+        String resp = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < head.size(); i++) {
+            resp = stringBuilder.toString();
+            stringBuilder.append(head.get(i)).append(PATTERN_SPLIT);
+            if(stringBuilder.length() > patternMaxLength){
+                patterns.add(resp.substring(0, resp.length() -1));
+                splitPattern(head.subList(i, head.size()), patterns);
+                return;
+            }
+        }
+        patterns.add(stringBuilder.substring(0, stringBuilder.length() -1));
     }
 
     public boolean filter(String filtered) throws CanalFilterException {
-        if (StringUtils.isEmpty(pattern)) {
+        if (CollectionUtils.isEmpty(patterns)) {
             return defaultEmptyValue;
         }
 
         if (StringUtils.isEmpty(filtered)) {
             return defaultEmptyValue;
         }
-
-        Map<String, Object> env = new HashMap<String, Object>();
-        env.put("pattern", pattern);
-        env.put("target", filtered.toLowerCase());
-        return (Boolean) exp.execute(env);
+        for (String pattern : patterns) {
+            Map<String, Object> env = new HashMap<String, Object>();
+            env.put("pattern", pattern);
+            env.put("target", filtered.toLowerCase());
+            Boolean resp = (Boolean) exp.execute(env);
+            if(resp){
+                return resp;
+            }
+        }
+        return false;
     }
 
     /**
      * 修复正则表达式匹配的问题，因为使用了 oro 的 matches，会出现：
-     * 
+     *
      * <pre>
      * foo|foot 匹配 foot 出错，原因是 foot 匹配了 foo 之后，会返回 foo，但是 foo 的长度和 foot 的长度不一样
      * </pre>
-     * 
+     * <p>
      * 因此此类对正则表达式进行了从长到短的排序
-     * 
+     *
      * @author zebin.xuzb 2012-10-22 下午2:02:26
      * @version 1.0.0
      */
@@ -102,13 +131,13 @@ public class AviaterRegexFilter implements CanalEventFilter<String> {
 
     /**
      * 修复正则表达式匹配的问题，即使按照长度递减排序，还是会出现以下问题：
-     * 
+     *
      * <pre>
      * foooo|f.*t 匹配 fooooot 出错，原因是 fooooot 匹配了 foooo 之后，会将 fooo 和数据进行匹配，但是 foooo 的长度和 fooooot 的长度不一样
      * </pre>
-     * 
+     * <p>
      * 因此此类对正则表达式进行头尾完全匹配
-     * 
+     *
      * @author simon
      * @version 1.0.0
      */
@@ -127,7 +156,7 @@ public class AviaterRegexFilter implements CanalEventFilter<String> {
 
     @Override
     public String toString() {
-        return pattern;
+        return patterns.toString();
     }
 
 }
